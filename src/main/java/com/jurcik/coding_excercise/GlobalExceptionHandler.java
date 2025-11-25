cd2a8c553b79e74a0acb11c329441cbc8cb398a5
martinjurcik <mar00jur@gmail.com>
2025-11-25 21:01:13 +0100

    minor fix

--- src/main/java/com/jurcik/coding_excercise/GlobalExceptionHandler.java
+++ src/main/java/com/jurcik/coding_excercise/GlobalExceptionHandler.java
@@ -5,13 +5,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.io.Serializable;
-
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ApiException.class)
-    public ResponseEntity<Serializable> handleCustomError(ApiException ex) {
-       return new ResponseEntity<Serializable>(ex.getMessage(), ex.getHttpStatus());
+    public ResponseEntity<String> handleCustomError(ApiException ex) {
+       return new ResponseEntity<>(ex.getMessage(), ex.getHttpStatus());
     }
 }
