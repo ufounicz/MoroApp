55d7dca09aa02e9c92863245bdc72d8ff5ab01fb
martinjurcik <mar00jur@gmail.com>
2025-11-25 14:33:36 +0100

    Initial commit

---
+++ src/main/java/com/jurcik/coding_excercise/CodingExcerciseApplication.java
@@ -0,0 +1,13 @@
+package com.jurcik.coding_excercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingExcerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingExcerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jurcik/coding_excercise/CodingExcerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jurcik.coding_excercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingExcerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
